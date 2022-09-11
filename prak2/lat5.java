26f8e8101436bb3733bbf9fe140fc71f7c530d66
Kiram <104960878+kiram02@users.noreply.github.com>
2022-09-11 11:04:57 +0700

    Create lat5.java

---
+++ prak2/lat5.java
@@ -0,0 +1,15 @@
+public class prak2Lat7 {
+
+	public static void main(String[] args) {
+		
+		for(int number=0; number<=20; number++){
+			
+			//check if number is even
+		 if(number%2==0){
+		     if(number%6 != 0){
+		         System.out.print(number+" ");
+		      }
+        }
+      }
+	  }
+}
