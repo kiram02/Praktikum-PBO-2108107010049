75a9c85fc520d6ff8e3a1f1968bf06bb117e1ae3
Kiram <104960878+kiram02@users.noreply.github.com>
2022-09-13 20:26:33 +0700

    Update lat4.java

--- prak2/lat4.java
+++ prak2/lat4.java
@@ -2,7 +2,9 @@ class lat4 {
     public static void main(String[] args) {
         int x = 20;
         for(int i = 1; i<=x; i++){
+            if(x % 2 == 0){
             System.out.print(" " + i);
+            }
         }
     }
 }
